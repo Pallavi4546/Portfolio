43c152e99f2ab5d9c9e1bbfa7c27390d66630c4d
Pallavi4546 <82462352+Pallavi4546@users.noreply.github.com>
2022-05-04 18:20:46 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: ['~assets/css/global.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["@/plugins/aos.js",],
+  plugins: ["@/Plugins/aos.js",],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
